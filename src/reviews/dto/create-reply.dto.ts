848f6ec6dac46c035d4813086ae8d48dd91ad2db
NisithaPadeniya <150885329+NisithaPadeniya@users.noreply.github.com>
2026-03-20 16:41:59 +0530

    Add CreateReplyDto and UpdateReplyDto for handling reply data

---
+++ src/reviews/dto/create-reply.dto.ts
@@ -0,0 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, Length } from 'class-validator';
+
+export class CreateReplyDto {
+  @ApiProperty({
+    description: 'Reply text',
+    minLength: 20,
+    maxLength: 1000,
+    example: 'Thank you for your feedback. We appreciate your comments.',
+  })
+  @IsString()
+  @Length(20, 1000, {
+    message: 'reply_text must be between 20 and 1000 characters',
+  })
+  reply_text: string;
+}
